e3fa412be16479e26f07c019ea274bc9a5ba1b3a
6r15u <grisu.der.kleine.Drache@gmx.de>
2018-01-06 16:25:52 +0100

    update to openHAB stable 2.2.0

--- shared/openHAB.sh
+++ shared/openHAB.sh
@@ -17,7 +17,7 @@ QPKG_STOP=${QPKG_DISTRIBUTION}/runtime/bin/stop
 QPKG_STATUS=${QPKG_DISTRIBUTION}/runtime/bin/status
 QPKG_CONSOLE=${QPKG_DISTRIBUTION}/start.sh
 QPKG_SNAPSHOT_FLAVOUR=offline
-QPKG_SNAPSHOT_VERSION=2.1.0
+QPKG_SNAPSHOT_VERSION=2.3.0
 
 function downloadJavaCommon {
     echo "Please visit http://www.oracle.com/technetwork/java/javase/terms/license/index.html"
